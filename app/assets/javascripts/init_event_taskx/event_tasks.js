12a10955a7c14c4f455ca5ef911ff82fdad36796
jc <emclab2011@gmail.com>
2014-07-25 11:24:08 -0500

    JC: added jquery and data picker to the engine. spec passes.

--- app/assets/javascripts/init_event_taskx/event_tasks.js
+++ app/assets/javascripts/init_event_taskx/event_tasks.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+   $("#event_task_start_date").datepicker({dateFormat: 'yy-mm-dd'});
+   $("#event_task_finish_date").datepicker({dateFormat: 'yy-mm-dd'});
+});
